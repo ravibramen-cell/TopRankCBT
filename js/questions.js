c2c5c1d13b9eb9eaf50695b4ddea2f6b00f428ab
ravibramen-cell <ravibramen@gmail.com>
2026-07-04 16:57:46 +0530

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,33 @@
+const questions = [
+
+{
+question:"She looked at ____ in the mirror.",
+options:["her","herself","hers","herself's"],
+answer:1
+},
+
+{
+question:"The Principal ____ addressed the meeting.",
+options:["himself","itself","herself","yourself"],
+answer:0
+},
+
+{
+question:"Either of the boys can do it.",
+options:["Reflexive","Distributive","Emphatic","Relative"],
+answer:1
+},
+
+{
+question:"We enjoyed ____ at the picnic.",
+options:["ourselves","us","our","ours"],
+answer:0
+},
+
+{
+question:"The CEO ____ signed the agreement.",
+options:["himself","herself","itself","ourselves"],
+answer:0
+}
+
+];
